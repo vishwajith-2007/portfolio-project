a0e3deb6c69aac785b0a960ee65ebe2ff63daeb7
vishwajith-2007 <jithusajitha7@gmail.com>
2026-02-26 14:51:53 +0530

    fixed backend url

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const name = document.getElementById("name").value
 const email = document.getElementById("email").value
 const message = document.getElementById("message").value
 
-fetch("http://localhost:3000/contact", {
+fetch("https://portfolio-project-iubg.onrender.com/contacts", {
 
 method: "POST",
 
